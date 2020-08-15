3479cb8027871db070e1b04f030cc796be3b1099
akshay-vinod <akshayvinod468@gmail.com>
2020-08-15 23:56:48 +0530

    safe level :)

---
+++ main.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+    $("#click").click(function(){
+        alert("Hai")
+    })
+})
+var x = document.getElementById("hai").value;
+document.getElementById('hello').onclick = function () {
+    //window.open('http://google.com/search?q=' + searchObject);
+    alert(x);
+    
+};*/
