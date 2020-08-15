15a4023711750962703cec6841406bd253e0ee6d
akshay-vinod <akshayvinod468@gmail.com>
2020-08-16 00:36:28 +0530

    all set nb:searching working finely

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-/*$(document).ready(function(){
-    $("#click").click(function(){
-        alert("Hai")
-    })
-})
-var x = document.getElementById("hai").value;
-document.getElementById('hello').onclick = function () {
-    //window.open('http://google.com/search?q=' + searchObject);
-    alert(x);
-    
-};*/
+function submit(){
+    document.getElementById('my_form').submit();
+}
